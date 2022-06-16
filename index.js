c66b361fbe04a9c5c00ca082d3f5b972acc7121c
Swordax <88690330+SwordaxSy@users.noreply.github.com>
2022-06-16 15:04:18 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * PotatoDB
+ * Copyrights(c) 2022-present Swordax
+ *
+ * discord: https://discord.com/users/465453058667839499/
+ * discord id: Swordax#5756
+ * github: https://github.com/SwordaxSy
+ * social media at: @swordax.sy
+ * email: swordax.sy@gmail.com
+ * website: https://swordax.netlify.app
+ *
+ * PotatoDB github repo: https://github.com/SwordaxSy/PotatoDB
+ */
+
+module.exports = require("./lib/potatodb.js");
